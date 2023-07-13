11761a360a06cf9ec1e03510096f0d3fc2121dd1
Deepayan2005 <106615192+Deepayan2005@users.noreply.github.com>
2023-07-13 08:34:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/details')
 def give_details():
     text = str(request.args.get('input'))
     page = requests.get(text)
@@ -30,3 +30,32 @@ def give_details():
     dict = {'components':compo,'servings':serving,'steps':steps}
     return json.dumps(dict)
 
+@app.route('/search')
+def search_recipes():
+    text = str(request.args.get('input'))
+    page = requests.get(text)
+    # Getting page HTML through request
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    data = soup.find('div', class_='clearfix nomrg recipe_like_listing') \
+        .find_all('div', class_='mustTry_left')
+    list = []
+    for rec in data:
+        try:
+            s2 = BeautifulSoup(str(rec), 'html.parser')
+            link = s2.find('span', class_='posrel').find('a').get('href')
+            image = s2.find('span', class_='posrel').find('a').find('img').get('data-src')
+            name = s2.find('div', class_='caption clearfix').find('h2').text.replace('\n', '')
+            time = s2.find('div', class_='caption clearfix').find('div', class_='nrecipe_vegnonveg') \
+                .text.replace('\n', '')
+            category = s2.find('div', class_='caption clearfix').find('div', class_='nrecipe_vegnonveg') \
+                .find('span', class_='vegnonveg').contents[0].__str__().replace('span', "").replace('<', '') \
+                .replace('>', '').replace('/', '').replace('"', '').replace('class', '').replace('=', '')
+            sen = {"name": name, "image": image, "link": link, "time": time, "category": category}
+
+            list.append(sen)
+
+        except AttributeError:
+            print("sorry")
+
+    return json.dumps(list)
